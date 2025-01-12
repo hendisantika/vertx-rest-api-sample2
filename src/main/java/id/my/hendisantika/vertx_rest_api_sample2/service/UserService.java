808aba0a381bd0fc5a4c005797e3ee5bde9699cc
hendisantika <hendisantika@yahoo.co.id>
2025-01-12 08:24:43 +0700

    Add UserService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vertx_rest_api_sample2/service/UserService.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.vertx_rest_api_sample2.service;
+
+import id.my.hendisantika.vertx_rest_api_sample2.repository.UserRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : vertx-rest-api-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/01/25
+ * Time: 08.24
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserService {
+  private final UserRepository userRepository = UserRepository.getInstance();
+}
