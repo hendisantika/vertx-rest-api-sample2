dfa4b0a18f321bc0f0e79b6b87657d6cc0adeaa2
hendisantika <hendisantika@yahoo.co.id>
2025-01-12 08:27:17 +0700

    Modify start🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vertx_rest_api_sample2/MainVerticle.java
+++ src/main/java/id/my/hendisantika/vertx_rest_api_sample2/MainVerticle.java
@@ -1,23 +1,51 @@
 package id.my.hendisantika.vertx_rest_api_sample2;
 
+import io.netty.handler.codec.http.HttpMethod;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Promise;
+import io.vertx.core.Future;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888).onComplete(http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
-    });
+  public void start(Future<Void> fut) {
+    Router router = Router.router(vertx); // <1>
+    // CORS support
+    Set<String> allowHeaders = new HashSet<>();
+    allowHeaders.add("x-requested-with");
+    allowHeaders.add("Access-Control-Allow-Origin");
+    allowHeaders.add("origin");
+    allowHeaders.add("Content-Type");
+    allowHeaders.add("accept");
+    Set<HttpMethod> allowMethods = new HashSet<>();
+    allowMethods.add(HttpMethod.GET);
+    allowMethods.add(HttpMethod.POST);
+    allowMethods.add(HttpMethod.DELETE);
+    allowMethods.add(HttpMethod.PUT);
+
+    router.route().handler(CorsHandler.create("*") // <2>
+      .allowedHeaders(allowHeaders)
+      .allowedMethods(allowMethods));
+    router.route().handler(BodyHandler.create()); // <3>
+
+    // routes
+    router.get("/position").handler(this::getPositions);
+    router.get("/user").handler(this::getUsers);
+    router.get("/user/:id").handler(this::getById);
+    router.post("/user").handler(this::save);
+    router.put("/user").handler(this::update);
+    router.delete("/user/:id").handler(this::remove);
+    router.post("/user/filter").handler(this::getUsersByFilter);
+
+    vertx.createHttpServer() // <4>
+      .requestHandler(router::accept)
+      .listen(8080, "0.0.0.0", result -> {
+        if (result.succeeded())
+          fut.complete();
+        else
+          fut.fail(result.cause());
+      });
   }
 }
